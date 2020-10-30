7ec3aa416649462bac7a20769fe7e3daa6858866
Hoon <kimhoon0316@gmail.com>
2020-10-30 18:06:34 +0900

    btn green css add

--- src/components/pages/SignIn.vue
+++ src/components/pages/SignIn.vue
@@ -1,21 +1,31 @@
 <template>
-  <form @submit="signIn">
-    <input
-      type="email"
-      name="email"
-      placeholder="Email..."
-      required
-      v-model="credentials.email"
-    />
-    <input
-      type="password"
-      name="password"
-      placeholder="password..."
-      required
-      v-model="credentials.password"
-    />
-    <button type="button" @click="signIn">Sign In</button>
-  </form>
+  <div class="grid grid-cols-1 pt-10 max-w-2xl text-center m-auto">
+    <div class="text-xl text-gray-700">SignIn</div>
+    <div class="text-5xl">Sign in your</div>
+    <form @submit="signIn">
+      <div class="flex flex-col mt-5">
+        <label for="email" class="text-left required">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email..."
+          required
+          v-model="credentials.email"
+        />
+      </div>
+      <div class="flex flex-col mt-5">
+        <label for="password" class="text-left required">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="password..."
+          required
+          v-model="credentials.password"
+        />
+      </div>
+      <button class="btn btn-green mt-10" @click="signIn">Sign In</button>
+    </form>
+  </div>
 </template>
 <script>
 import { auth } from "../../compositions/auth";
